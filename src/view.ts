d51363320b02712bf5532677e8b85ca250594b3f
Thomas Manning <thomasm@balena.io>
2022-09-19 01:41:27 +1000

    Add time since price was checked
    So that out-of-date prices are obvious

--- src/view.ts
+++ src/view.ts
@@ -1,6 +1,7 @@
 import _ from "lodash";
 import {Catalogue} from "./catalogue.js";
 import {Low} from "lowdb";
+import humanizeDuration from "humanize-duration";
 
 export function shoppingList (db: Low<Catalogue>) {
     let idx = 0
@@ -11,11 +12,13 @@ export function shoppingList (db: Low<Catalogue>) {
             const numSkus = product.skus.length
             let salePct = 0
             let price = 'Not found'
+            let since = NaN
             if (min) {
                 price = '$' + min.price.toFixed(2)
                 salePct = (1 - (min.price / product.normalPrice)) * 100
+                since = Date.now() - new Date(min.at).valueOf()
             }
-            return {...product, salePct, price, normalPrice, numSkus}
+            return {...product, salePct, price, normalPrice, numSkus, since: Number.isNaN(since) ? '-': humanizeDuration(since, { largest: 1 }) }
         }),
         idx: () => ++idx
     }
